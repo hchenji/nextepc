089f9d122da66b53fa4f1562c12ec903e3eaa539
Harsha Chenji <chenji@ohio.edu>
2019-11-04 19:07:30 -0500

    fixed some config bugs in freedia

--- dnet/settun.sh
+++ dnet/settun.sh
@@ -3,15 +3,15 @@
 mkdir -p /dev/net
 mknod /dev/net/tun c 10 200
 
-if ! grep "pgwtun" /proc/net/dev > /dev/null; then
+if ! grep "ogstun" /proc/net/dev > /dev/null; then
     ip tuntap add name ogstun mode tun
 fi
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-iptables -I INPUT -i pgwtun -j ACCEPT
-iptables -A FORWARD -i pgwtun -o eth0 -j ACCEPT
-iptables -A FORWARD -i eth0 -o pgwtun -j ACCEPT
-ip addr del $NEPC_PGW_IP4/$NEPC_PGW_MASK4 dev pgwtun 2> /dev/null
-ip addr add $NEPC_PGW_IP4/$NEPC_PGW_MASK4 dev pgwtun
-# ip addr del $NEPC_PGW_IP6/$NEPC_PGW_MASK6 dev pgwtun 2> /dev/null
-# ip addr add $NEPC_PGW_IP6/$NEPC_PGW_MASK6 dev pgwtun
-ip link set pgwtun up
+iptables -I INPUT -i ogstun -j ACCEPT
+iptables -A FORWARD -i ogstun -o eth0 -j ACCEPT
+iptables -A FORWARD -i eth0 -o ogstun -j ACCEPT
+ip addr del $NEPC_PGW_IP4/$NEPC_PGW_MASK4 dev ogstun 2> /dev/null
+ip addr add $NEPC_PGW_IP4/$NEPC_PGW_MASK4 dev ogstun
+# ip addr del $NEPC_PGW_IP6/$NEPC_PGW_MASK6 dev ogstun 2> /dev/null
+# ip addr add $NEPC_PGW_IP6/$NEPC_PGW_MASK6 dev ogstun
+ip link set ogstun up
